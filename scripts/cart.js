a920ed09f9ac067be3b71b67acb242458c4175fd
avgusr <goodcrowsareviolet@protonmail.com>
2024-05-09 04:11:18 -0400

    Terminada la lógica de cookies. Falta procesarlas.

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,10 +1,31 @@
 $(document).ready(function(){
+  cookieProcess(readCookie("cart_items"))
+  elementExistsValidation();
   $('.product-quantity-select').on('input', function() {
     var sanitized = $(this).val().replace(/[^0-9]/g, "1");
     $(this).val(sanitized);
   });
+  $('.product-item-delete').click(function(event) {
+    event.preventDefault();
+    
+    var productItem = $(this).closest('.product-item');
+  
+    var productId = "#" + productItem.attr('id');
+  
+    $(productId).nextAll(".product-sep").first().remove()
+    $(productId).remove();
+    
+    elementExistsValidation();
+  });
 });
 
-function deleteFromCar() {
-  
+function elementExistsValidation(){
+  var listItems = document.querySelectorAll('.product-item');
+  var count = listItems.length;
+  if (count == 0){
+    document.getElementById("no-items").style.display = "flex";
+  } else {
+    document.getElementById("no-items").style.display = "none";
+  }
 }
+
--- scripts/utils.js
+++ scripts/utils.js
@@ -3,3 +3,52 @@ function isEmailValid(emailString){
 
     return emailRegex.test(emailString);
 }
+
+function cookieProcess(cookie_value){
+  
+    var actualChar;
+    var productID = "";
+    var productQuantity = "";
+    var fetchingID = true;
+    var products = new Map();
+  
+    for (let i = 0; i < cookie_value.length; i++) {
+      actualChar = cookie_value.charAt(i);
+      if(actualChar == '%'){
+        fetchingID = false;
+      } else if (actualChar == '&'){
+        products.set(productID,productQuantity);
+        productID = "";
+        productQuantity = "";
+        fetchingID = true;
+      } else if (isNumeric(actualChar)) {
+        if (fetchingID){
+          productID += actualChar;
+        } else {
+          productQuantity += actualChar; 
+        };
+      };
+    }
+    for (let [key, value] of products) {
+        // Mostrar el nombre de la clave y su valor en un alert
+        alert(`Producto: ${key}, Cantidad: ${value}`);
+      }
+  }
+  
+function readCookie(name)
+{
+name += '=';
+var parts = document.cookie.split(/;\s*/);
+for (var i = 0; i < parts.length; i++)
+{
+    var part = parts[i];
+    if (part.indexOf(name) == 0)
+    return part.substring(name.length)
+}
+return null;
+}
+  
+function isNumeric(str) {
+if (typeof str != "string") return false
+return !isNaN(str) && !isNaN(parseFloat(str))
+}
